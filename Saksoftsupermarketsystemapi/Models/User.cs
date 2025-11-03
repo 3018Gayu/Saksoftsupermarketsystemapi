fddd4bd654b4a824a837358931b0e5f470822f18
Gayathri <gayathrialagiri2003@gmail.com>
2025-11-03 17:18:00 +0530

    Add project files.

---
+++ Saksoftsupermarketsystemapi/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Saksoftsupermarketsystemapi.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string UserEmail { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public string UserRole { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+}
